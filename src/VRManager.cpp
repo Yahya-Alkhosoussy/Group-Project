5a380cb102e432415eec04e213c2a6609bb0a344
Yahya Alkhosoussy <97708352+Yahya-Alkhosoussy@users.noreply.github.com>
2026-04-28 17:22:55 +0100

    Finished the start and stop functions

--- src/VRManager.cpp
+++ src/VRManager.cpp
@@ -1,6 +1,7 @@
 #include "VRManager.h"
 
 #include <QTimer>
+#include <QDir>
 
 #include <vtkOpenVRRenderWindow.h>
 #include <vtkOpenVRCamera.h>
@@ -29,13 +30,65 @@ VRManager::~VRManager() { stop(); }
 
 bool VRManager::start(const QString& manifestDir) {
     if (m_impl->active) { return true; } // The VR side is already online
-    vtkNew<vtkRenderer> renderer;
-	vtkNew <vtkOpenVRRenderWindow> window;
-    return false;
+    
+	if (!QDir(manifestDir).exists() || !QDir(manifestDir).exists("vtk_openvr_actions.json")) { 
+		emit vrError("Required Files do not exist.");
+		return false; 
+	} // required files aren't available
+	
+	m_impl->renderer = vtkSmartPointer<vtkRenderer>::New();
+
+	m_impl->renderWindow = vtkSmartPointer<vtkOpenVRRenderWindow>::New();
+	m_impl->renderWindow->AddRenderer(m_impl->renderer);
+
+	m_impl->camera = vtkSmartPointer<vtkOpenVRCamera>::New();
+	m_impl->renderer->SetActiveCamera(m_impl->camera);
+
+	m_impl->interactor = vtkSmartPointer<vtkOpenVRRenderWindowInteractor>::New();
+	m_impl->interactor->SetRenderWindow(m_impl->renderWindow);
+	m_impl->interactor->SetActionManifestDirectory(manifestDir.toStdString());
+	m_impl->interactor->SetActionManifestFileName("vtk_openvr_actions.json");
+
+	m_impl->style = vtkSmartPointer<vtkOpenVRInteractorStyle>::New();
+	m_impl->interactor->SetInteractorStyle(m_impl->style);
+
+	m_impl->interactor->Initialize();
+	if (m_impl->renderWindow->GetHMD() == nullptr) {
+		m_impl->interactor->TerminateApp();
+		m_impl->style			= nullptr;
+		m_impl->interactor		= nullptr;
+		m_impl->camera			= nullptr;
+		m_impl->renderWindow	= nullptr;
+		m_impl->renderer		= nullptr;
+		emit vrError("Could not start VR session");
+		return false;
+	}
+
+	m_impl->active = true;
+	m_impl->timer->start();
+	emit vrStarted("Started the VR session");
+	return true;
 }
 
 void VRManager::stop() {
-	// to be filled in
+	// make sure the VR app is open
+	if (!m_impl->active) {
+		return;
+	}
+	m_impl->timer->stop(); // stopping QT's timer
+
+	clearActors(); // remove all the actors before nullifying the pointers
+
+	// removing all the pointers in reverse order.
+	m_impl->interactor->TerminateApp();
+	m_impl->style = nullptr;
+	m_impl->interactor = nullptr;
+	m_impl->camera = nullptr;
+	m_impl->renderWindow = nullptr;
+	m_impl->renderer = nullptr;
+
+	m_impl->active = false;
+	emit vrStopped("Stopped the VR Application");
 }
 
 bool VRManager::isActive() const { return m_impl->active; }
@@ -50,4 +103,8 @@ void VRManager::addActor(vtkActor* actor) {
 
 void VRManager::removeActor(vtkActor* actor) {
 	// later
+}
+
+void VRManager::clearActors() {
+
 }
