fb907d51169ea0f432717289b1d4bd5ec2ca4527
Yahya-Alkhosoussy <97708352+Yahya-Alkhosoussy@users.noreply.github.com>
2026-05-04 23:10:14 +0000

    Deploying to gh-pages from @ Yahya-Alkhosoussy/Group-Project@78b906c87fcce9dd9a86bbf49b15dc62d0f1a498 🚀

--- classVRInteractorStyle.js
+++ classVRInteractorStyle.js
@@ -1,6 +1,7 @@
 var classVRInteractorStyle =
 [
+    [ "handleLeftGrip", "classVRInteractorStyle.html#a5fc19de249712b4a99bbcbb98e22e0b4", null ],
+    [ "handleRightGrip", "classVRInteractorStyle.html#a01b2e2e6e0321cd973283d260ce64520", null ],
     [ "OnMenu3D", "classVRInteractorStyle.html#a419a55189ebab324a58f282e268cd672", null ],
-    [ "OnNextPose3D", "classVRInteractorStyle.html#acdb02ce93bfb9dc8e3ccb8c4f23c84c0", null ],
     [ "setManager", "classVRInteractorStyle.html#a3ab739ffde380aac0e4bad7b5738d139", null ]
 ];
--- search/all_5.js
+++ search/all_5.js
@@ -1,10 +1,12 @@
 var searchData=
 [
   ['handleclearbutton_0',['handleClearButton',['../classMainWindow.html#a0dbf08b38bdcb6d3a067dbe138bb580a',1,'MainWindow']]],
-  ['handleresetviewbutton_1',['handleResetViewButton',['../classMainWindow.html#aea0e836898d63c285caaca1982913378',1,'MainWindow']]],
-  ['handleshowallbutton_2',['handleShowAllButton',['../classMainWindow.html#a1fa3abe0257c802e6989cc8a95484b12',1,'MainWindow']]],
-  ['handletogglevrbutton_3',['handleToggleVRButton',['../classMainWindow.html#a63a27638db6083c8d7d06e5327e116d6',1,'MainWindow']]],
-  ['handletransparencyslider_4',['handleTransparencySlider',['../classMainWindow.html#ab44b7dbdc39f9e200f6b8dae8e9baa16',1,'MainWindow']]],
-  ['handletreeclicked_5',['handleTreeClicked',['../classMainWindow.html#abfa3467a6bf77d8c3b0197025b41ab2d',1,'MainWindow']]],
-  ['headerdata_6',['headerData',['../classModelPartList.html#a6a54464c7907f0c18c2f5edd7cb4a51e',1,'ModelPartList']]]
+  ['handleleftgrip_1',['handleLeftGrip',['../classVRInteractorStyle.html#a5fc19de249712b4a99bbcbb98e22e0b4',1,'VRInteractorStyle']]],
+  ['handleresetviewbutton_2',['handleResetViewButton',['../classMainWindow.html#aea0e836898d63c285caaca1982913378',1,'MainWindow']]],
+  ['handlerightgrip_3',['handleRightGrip',['../classVRInteractorStyle.html#a01b2e2e6e0321cd973283d260ce64520',1,'VRInteractorStyle']]],
+  ['handleshowallbutton_4',['handleShowAllButton',['../classMainWindow.html#a1fa3abe0257c802e6989cc8a95484b12',1,'MainWindow']]],
+  ['handletogglevrbutton_5',['handleToggleVRButton',['../classMainWindow.html#a63a27638db6083c8d7d06e5327e116d6',1,'MainWindow']]],
+  ['handletransparencyslider_6',['handleTransparencySlider',['../classMainWindow.html#ab44b7dbdc39f9e200f6b8dae8e9baa16',1,'MainWindow']]],
+  ['handletreeclicked_7',['handleTreeClicked',['../classMainWindow.html#abfa3467a6bf77d8c3b0197025b41ab2d',1,'MainWindow']]],
+  ['headerdata_8',['headerData',['../classModelPartList.html#a6a54464c7907f0c18c2f5edd7cb4a51e',1,'ModelPartList']]]
 ];
